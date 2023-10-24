4cbc398f5ce0a15382e544f732c2a8067585d071
Esneyder Galviz <119534032+gitgalviz@users.noreply.github.com>
2023-10-23 19:50:45 -0500

    Add files via upload

--- formulario/1.js
+++ formulario/1.js
@@ -1,115 +1,208 @@
 let informacion = [];
+let op = null;
+let indice = null;
 
-function guardar() {
+function validar() {
+    let nombre = document.getElementById("nombres").value;
+    let apellido = document.getElementById("apellidos").value;
+    let tipoDoc = document.querySelector('input[name="tipo_documento"]:checked');
+    let doc = document.getElementById("numero_documento").value;
+    let genero = document.querySelector('input[name="genero"]:checked');
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+    let fecha = document.getElementById("fecha_nacimiento").value;
+    let notificaciones = document.getElementById("notificaciones").checked;
 
-    const valido = validar();
+    if (nombre === "") {
+        mostrarAlerta("Ingrese un nombre");
+        return;
+    }
+    if (apellido === "") {
+        mostrarAlerta("Ingrese un apellido");
+        return;
+    }
+    if (tipoDoc === null) {
+        mostrarAlerta("Seleccione un tipo de documento");
+        return;
+    }
+    if (doc === "") {
+        mostrarAlerta("Ingrese un número de documento");
+        return;
+    }
+    if (genero === null) {
+        mostrarAlerta("Seleccione uno de los generos");
+        return;
+    }
+    if (fecha === "") {
+        mostrarAlerta("Ingrese la fecha de nacimiento");
+        return;
+    } else {
+        let fechaNacimiento = new Date(fecha);
+        let hoy = new Date();
+        let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+        let mes = hoy.getMonth() - fechaNacimiento.getMonth();
+        if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+            edad--;
+        }
+        if (edad < 18) {
+            mostrarAlerta("Debe ser mayor de 18 años para registrarse.");
+            return;
+        }
+    }
+    if (telefono === "") {
+        mostrarAlerta("Ingrese un número de telefono");
+        return;
+    } else if (telefono.length !== 10 || isNaN(telefono)) {
+        mostrarAlerta("El número de teléfono debe tener 10 dígitos.");
+        return;
+    }
+    if (correo === "") {
+        mostrarAlerta("Ingrese una dirección de correo");
+        return;
+    } else {
+        let correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!correoRegex.test(correo)) {
+            mostrarAlerta("Por favor, introduzca una dirección de correo válida.");
+            return;
+        }
+    }
+    if (op === true) {
 
-    if (valido) {
-        const nombre = document.getElementById("nombres").value;
-        const apellido = document.getElementById("apellidos").value;
-        const tipoDocumento = document.querySelector('input[name="tipo_documento"]:checked').value;
-        const numeroDocumento = document.getElementById("numero_documento").value;
-        const genero = document.querySelector('input[name="genero"]:checked').value;
-        const fechaNacimiento = document.getElementById("fecha_nacimiento").value;
-        const telefono = document.getElementById("telefono").value;
-        const correo = document.getElementById("correo").value;
-        const recibirNotificaciones = document.getElementById("notificaciones").checked;
-        const hoy = new Date();
-        const fechaNacimientoDate = new Date(fechaNacimiento);
+        informacion[indice].nombre = document.getElementById("nombres").value;
+        informacion[indice].apellido = document.getElementById("apellidos").value;
+        informacion[indice].tipoDoc = document.querySelector('input[name="tipo_documento"]:checked').value;
+        informacion[indice].doc = document.getElementById("numero_documento").value;
+        informacion[indice].genero = document.querySelector('input[name="genero"]:checked').value;
+        informacion[indice].telefono = document.getElementById("telefono").value;
+        informacion[indice].correo = document.getElementById("correo").value;
+        informacion[indice].fecha = document.getElementById("fecha_nacimiento").value;
+    } else {
 
-        const persona = {
+        let tipoDocValue = tipoDoc.value;
+        let generoValue = genero.value;
+
+        let persona = {
             nombre: nombre,
             apellido: apellido,
-            tipoDocumento: tipoDocumento,
-            numeroDocumento: numeroDocumento,
-            genero: genero,
-            fechaNacimiento: fechaNacimiento,
+            tipoDocumento: tipoDocValue,
+            numeroDocumento: doc,
+            genero: generoValue,
+            fechaNacimiento: fecha,
             telefono: telefono,
             correo: correo,
-            recibirNotificaciones: recibirNotificaciones
+            Notificaciones: notificaciones,
+
         };
 
         informacion.push(persona);
 
-        console.log(persona);
-        document.getElementById("alertaadso").textContent = "Su información fue registrada correctamente";
+        alert("Información guardada con exito");
+
     }
+    document.getElementById("nombres").value = "";
+    document.getElementById("apellidos").value = "";
+    document.querySelector('input[name="tipo_documento"]:checked').checked = false;
+    document.getElementById("numero_documento").value = "";
+    document.querySelector('input[name="genero"]:checked').checked = false;
+    document.getElementById("telefono").value = "";
+    document.getElementById("correo").value = "";
+    document.getElementById("fecha_nacimiento").value = "";
+    document.getElementById("notificaciones").checked = false;
+
+    document.getElementById("tabla").innerHTML = "";
+    insertar();
 }
 
-function validar() {
-    let valido = true;
-    const nombre = document.getElementById("nombres").value;
-    const apellido = document.getElementById("apellidos").value;
-    const tipoDocumento = document.querySelector('input[name="tipo_documento"]:checked');
-    const numeroDocumento = document.getElementById("numero_documento").value;
-    const genero = document.querySelector('input[name="genero"]:checked');
-    const fechaNacimiento = document.getElementById("fecha_nacimiento").value;
-    const telefono = document.getElementById("telefono").value;
-    const correo = document.getElementById("correo").value;
-    const hoy = new Date();
-    const fechaNacimientoDate = new Date(fechaNacimiento);
-
-    let alerta = "Por favor complete los siguientes campos correctamente: ";
-
-    if (nombre === "") {
-        alerta += "Nombre, ";
-        valido = false;
-    }
-
-    if (apellido === "") {
-        alerta += "Apellido, ";
-        valido = false;
-    }
-
-    if (!tipoDocumento) {
-        alerta += "Tipo de documento, ";
-        valido = false;
-    }
-
-    if (numeroDocumento === "") {
-        alerta += "Número de documento, ";
-        valido = false;
-    }
-
-    if (!genero) {
-        alerta += "Género, ";
-        valido = false;
-    }
-
-    if (fechaNacimiento === "") {
-        alerta += "Fecha de nacimiento, ";
-        valido = false;
-    } else {
-        const edad = hoy.getFullYear() - fechaNacimientoDate.getFullYear();
-        if (edad < 18) {
-            alerta += "Debe ser mayor de 18 años, ";
-            valido = false;
-        }
-    }
-
-    if (telefono === "") {
-        alerta += "Teléfono, ";
-        valido = false;
-    }
-
-    if (correo === "") {
-        alerta += "Correo electrónico, ";
-        valido = false;
-    } else if (!correo.includes("@")) {
-        alerta += "Correo electrónico debe contener el símbolo '@', ";
-        valido = false;
-    }
-
-    if (!valido) {
-        document.getElementById("alertaadso").textContent = alerta;
-        setTimeout(() => {
-            document.getElementById("alertaadso").textContent = "";
-        }, 6000);
-    }
-
-    return valido;
+function mostrarAlerta(mensaje) {
+    document.getElementById("alertaadso").textContent = mensaje;
+    setTimeout(() => {
+        document.getElementById("alertaadso").textContent = "";
+    }, 6000);
 }
 
+function insertar() {
+    let cardContainer = document.getElementById("cont_cards");
+    cardContainer.innerHTML = "";
 
+    informacion.forEach((item, index) => {
+        let card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+                <h1>${item.nombre}</h1>
+                <h1>${item.apellido}</h1>
+                <p><strong>Tipo de documento: </strong> ${item.tipoDocumento}</p>
+                <p><strong>Número de documento: </strong> ${item.numeroDocumento}</p>
+                <p><strong>Género: </strong> ${item.genero}</p>
+                <p><strong>Fecha de nacimiento: </strong> ${item.fechaNacimiento}</p>
+                <p><strong>Teléfono: </strong> ${item.telefono}</p>
+                <p><strong>Correo: </strong> ${item.correo}</p>
+            `;
+        cardContainer.appendChild(card);
+    })
 
+    let frag = document.createDocumentFragment();
 
+    informacion.forEach((item, index) => {
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        let td4 = document.createElement("td");
+        let td5 = document.createElement("td");
+        let td6 = document.createElement("td");
+        let td7 = document.createElement("td");
+        let td8 = document.createElement("td");
+        let td9 = document.createElement("td");
+        let editar = document.createElement("button");
+        let eliminar = document.createElement("button");
+        editar.textContent = "📝";
+        editar.addEventListener("click", () => {
+            Editar(item, index);
+        })
+        eliminar.textContent = "❌";
+        eliminar.addEventListener("click", () => {
+            Eliminar(index);
+        });
+        td1.textContent = item.nombre;
+        td2.textContent = item.apellido;
+        td4.textContent = item.tipoDocumento;
+        td3.textContent = item.numeroDocumento;
+        td5.textContent = item.genero;
+        td6.textContent = item.fechaNacimiento;
+        td7.textContent = item.telefono;
+        td8.textContent = item.correo;
+        td9.appendChild(editar);
+        td9.appendChild(eliminar);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+        tr.appendChild(td7);
+        tr.appendChild(td8);
+        tr.appendChild(td9);
+        frag.appendChild(tr);
+    });
+    let tabla = document.getElementById("tabla");
+    tabla.innerHTML = "";
+    tabla.appendChild(frag);
+
+    function Editar(r, i) {
+        op = true;
+        indice = i;
+        document.getElementById("nombres").value = r.nombre;
+        document.getElementById("apellidos").value = r.apellido;
+        document.querySelector('input[name="tipo_documento"][value="' + r.tipoDocumento + '"]').checked = true;
+        document.getElementById("numero_documento").value = r.numeroDocumento;
+        document.querySelector('input[name="genero"][value="' + r.genero + '"]').checked = true;
+        document.getElementById("telefono").value = r.telefono;
+        document.getElementById("correo").value = r.correo;
+        document.getElementById("fecha_nacimiento").value = r.fechaNacimiento;
+    }
+
+    function Eliminar(i) {
+        informacion.splice(i, 1);
+        insertar();
+    }
+}
